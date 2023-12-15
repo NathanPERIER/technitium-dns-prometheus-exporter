c1bebb7d6d8d271b6ff5ccfacd27db556e7b300a
NathanPERIER <periernathan2000@gmail.com>
2023-12-15 01:37:13 +0100

    Fixed error management and cached count

--- src/technitium-dns/requests.ts
+++ src/technitium-dns/requests.ts
@@ -8,35 +8,60 @@ import { ApiStatus, MetricsPoint } from './datastruct/metrics_point.js';
 const API_METRICS_PATH = 'api/dashboard/stats/get';
 
 type MetricsResponse = {
-    status: string,
-    response: {
-        stats: {
-            totalQueries:       number,
-            totalNoError:       number,
-            totalServerFailure: number,
-            totalNxDomain:      number,
-            totalRefused:       number,
-            totalAuthoritative: number,
-            totalRecursive:     number,
-            totalCached:        number,
-            totalBlocked:       number,
-            totalClients:       number,
-            zones:              number,
-            cachedEntries:      number,
-            allowedZones:       number,
-            blockedZones:       number,
-            allowListZones:     number,
-            blockListZones:     number,
-        },
-        mainChartData: {
-            labels: string[],
-            datasets: {
-                label: string,
-                data: number[]
-            }[]
-        }
+    stats: {
+        totalQueries:       number,
+        totalNoError:       number,
+        totalServerFailure: number,
+        totalNxDomain:      number,
+        totalRefused:       number,
+        totalAuthoritative: number,
+        totalRecursive:     number,
+        totalCached:        number,
+        totalBlocked:       number,
+        totalClients:       number,
+        zones:              number,
+        cachedEntries:      number,
+        allowedZones:       number,
+        blockedZones:       number,
+        allowListZones:     number,
+        blockListZones:     number,
+    },
+    mainChartData: {
+        labels: string[],
+        datasets: {
+            label: string,
+            data: number[]
+        }[]
     }
 };
+
+
+async function get_raw_body(base_path: string, token: string): Promise<[ApiStatus,MetricsResponse?]> {
+    try {
+        const rsp = await axios.get<{status: string, response: MetricsResponse}>(path.join(base_path, API_METRICS_PATH), {
+            params: { 'token': token, 'type': 'lastHour', 'utc': 'true' },
+            validateStatus: null
+        });
+        if(rsp.status < 200 || rsp.status >= 300) {
+            console.error(`Got HTTP return code ${rsp.status} ${rsp.statusText} on request to "${base_path}"`);
+            return [ApiStatus.HTTP_ERROR, undefined];
+        }
+        const body = rsp.data;
+        if(body.status != 'ok') {
+            console.warn(`Got error status "${rsp.status}" from API on request to "${base_path}"`);
+            return [ApiStatus.API_ERROR, undefined];
+        }
+        return [ApiStatus.OK, body.response];
+    } catch (err) {
+        if(err instanceof axios.AxiosError) {
+            console.warn(`Got error from Axios during a request to "${base_path}" : ${err.message}`);
+        } else {
+            console.warn(`Got unknown error of type ${typeof err} during a request to "${base_path}"`);
+        }
+        return [ApiStatus.UNREACHABLE, undefined];
+    }
+    
+}
 
 
 export async function fetch_from_api(base_path: string, token: string): Promise<MetricsPoint> {
@@ -60,28 +85,20 @@ export async function fetch_from_api(base_path: string, token: string): Promise<
             blocked: 0
         }
     };
-    const rsp = await axios.get<MetricsResponse>(path.join(base_path, API_METRICS_PATH), {
-        params: { 'token': token, 'type': 'lastHour', 'utc': 'true' }
-    });
-    if(rsp.status < 200 || rsp.status >= 300) {
-        console.error(`Got HTTP return code ${rsp.status} ${rsp.statusText} on request to "${base_path}"`);
-        res.status = ApiStatus.HTTP_ERROR;
+    const [status, response] = await get_raw_body(base_path, token);
+    res.status = status;
+    if(res.status != ApiStatus.OK) {
         return res;
     }
-    const body = rsp.data;
-    if(body.status != 'ok') {
-        console.warn(`Got error status "${rsp.status}" from API on request to "${base_path}"`);
-        res.status = ApiStatus.API_ERROR;
-        return res;
-    }
-    const stats = body.response.stats;
+    const stats = response!.stats;
     res.cached_entries = stats.cachedEntries;
     res.nb_zones = stats.zones;
     res.allowed_zones = stats.allowedZones;
     res.blocked_zones = stats.blockedZones;
     res.allow_list_zones = stats.allowListZones;
     res.block_list_zones = stats.blockListZones;
-    for(const dataset_points of body.response.mainChartData.datasets) {
+    const chart_data = response!.mainChartData;
+    for(const dataset_points of chart_data.datasets) {
         const val = dataset_points.data[dataset_points.data.length - 2];
         switch(dataset_points.label) {
             case 'Total': break;
@@ -91,13 +108,13 @@ export async function fetch_from_api(base_path: string, token: string): Promise<
             case 'Refused':        res.hits.refused = val;        break;
             case 'Authoritative':  res.hits.authoritative = val;  break;
             case 'Recursive':      res.hits.recursive = val;      break;
-            case 'Cached':         res.cached_entries = val;      break;
+            case 'Cached':         res.hits.cached = val;         break;
             case 'Blocked':        res.hits.blocked = val;        break;
             case 'Clients':        res.nb_clients = val;          break;
             default: console.warn(`Got unknown chart point type ${dataset_points.label}`);
         }
     }
-    const date = body.response.mainChartData.labels[body.response.mainChartData.labels.length - 2];
+    const date = chart_data.labels[chart_data.labels.length - 2];
     console.info(`Request to ${base_path} completed, got point for ${date}`);
     return res;
 };
